49b27491e395a72dca6eb75ee613ca3e624a8bab
André pierre <temps.jo@gmail.com>
2014-12-10 12:56:25 +0100

    Create monmix.h

---
+++ monmix.h
@@ -0,0 +1,65 @@
+#ifndef DEF_MONMIX
+#define DEF_MONMIX
+#include <iostream>
+#include <iostream>
+#include <sys/types.h>
+#include <signal.h>
+#include <unistd.h>
+#include "convertisseur.h"
+#include "AdnForme.h"
+#include "pourleger.h"
+/*#include "AdnForme1.h"
+#include "AdnForme2.h"
+#include "AdnForme3.h"
+#include "AdnForme4.h"
+#include "AdnForme5.h"
+#include "AdnForme6.h"
+#include "AdnForme7.h"*/
+using namespace std;
+class MonMix 
+{
+
+
+public:
+MonMix();
+private:
+
+
+public:
+void ouvrirCompose1();
+void ouvrirCompose2();
+void ouvrirCompose3();
+void ouvrirCompose4();
+void ouvrirCompose5();
+void ouvrirCompose6();
+void ouvrirCompose7();
+void ouvrirCompose8();
+void ouvrirCompose9();
+void ouvrirCompose10();
+void ouvrirCompose11();
+void ouvrirCompose12();
+void ouvrirCompose13();
+void ouvrirCompose14();
+void ouvrirCompose15();
+void ouvrirCompose16();
+void ouvrirMix1();
+void ouvrirMix2();
+void ouvrirMix3();
+void ouvrirMix4();
+void ouvrirMix5();
+void ouvrirMix6();
+void ouvrirMix7();
+void ouvrirMix8();
+void ouvrirMix9();
+void ouvrirMix10();
+void ouvrirMix11();
+void ouvrirMix12();
+void ouvrirMix13();
+void ouvrirMix14();
+void ouvrirMix15();
+void ouvrirMix16();
+void effaceMix16();
+
+
+};
+#endif // MIX_H
